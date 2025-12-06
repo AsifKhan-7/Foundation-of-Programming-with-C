83e78d135e1579ca585894be3502a4a81afe5025
Asif Khan <asifjamshed4@gmail.com>
2025-12-06 22:56:35 +0530

    Exercise 1 is complete

---
+++ 1/exercise.c
@@ -0,0 +1,50 @@
+
+#include<stdio.h>
+
+int main() {
+  /* WAP to print Hello Students on the screen*/
+  // printf("Hello Students");
+
+  /* WAP to print Hello in the first line and Students in the second line*/
+  // printf("Hello\nStudents");
+
+  /* WAP to print MySirG on the screen(Print double quotes)*/
+  //printf("\"MySirG\""); --- ("\"Text\"") -> Print double quotes on the text
+
+  /* WAP to print \n on the */
+  // printf("\\n");
+
+  /* WAP to print \r on the */
+  // printf("\\n");
+
+  /* WAP to print "Teacher's Day" on the screen(Print double & single quotes)*/
+  // printf("\"Teacher's Day\"");
+
+  /*WAP to calculate sum of two integers. Integers are taken from user through keyboard*/
+  /*int a,b;
+  printf("Please enter two numbers: ");
+  scanf("%d %d", &a,&b);
+  printf("Sum of %d and %d is %d", a, b, a+b); */
+
+  /*WAP to calculate square of a given number. Numbers are taken from user through keyboard*/
+  /*int a;
+  printf("Please enter the number: ");
+  scanf("%d", &a);
+  printf("Square of %d is %d", a, a*a);*/
+
+  /*WAP to find the area of a rectangle  Input appropriate data from user*/
+  /*float l,w;
+  printf("Please enter length and width: ");
+  scanf("%f %f, &l,&w);
+  printf("Area of rectangle  %f and %f is %f", l, w, l*w);*/
+
+  /*WAP to find the area of circle.  Take radius of circle from user*/
+  /*float r;
+  printf("Please enter length and width: ");
+  scanf("%f",  &r);
+  printf("Area of circle  %f is %f", r, 3.14 *r *r);*/
+
+
+  printf("Exercise 1 is complete");
+  return 0;
+}
