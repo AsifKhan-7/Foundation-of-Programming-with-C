5c9e14595634a912eb4ea11f27f9a0a829486b2e
Asif Khan <asifjamshed4@gmail.com>
2025-12-10 23:32:50 +0530

    Exercise - 4 is continuing...

--- 2/exercise4.c
+++ 2/exercise4.c
@@ -13,13 +13,43 @@ int main() {
       sizeOfCharacter = sizeof(character);
       printf("Size of character %c is: %d ",character, sizeOfCharacter);*/
 
-      //WAP to to calculate size of a Real constant
 
-      /*float realConstant, sizeOfRealConstant;
+      //WAP to calculate size of a Real constant
+
+      /*double realConstant;
+      int sizeOfRealConstant;
       printf("Please enter any real constant: ");
-      scanf("%f", &realConstant);
+      scanf("%lf", &realConstant);
       sizeOfRealConstant = sizeof(realConstant);
-      printf("Size of real constant %f is: %lf", realConstant, sizeOfRealConstant);*/
+      printf("Size of real constant %lf is: %d", realConstant, sizeOfRealConstant);*/
+
+
+      //WAP with one char type variable. Assign 'A' in the variable. Now change the value of variable from 'A' to 'B' using increment operator.
+
+      /*char typeOfVariable = 'A';
+      typeOfVariable++;
+      printf("Now the changed character is: %c", typeOfVariable);*/
+
+      //WAP to swap values of two int variables
+
+      /*int a = 65, b = 66, temp;
+      temp = a;
+      a = b;
+      b = temp;
+      printf("Now a is: %d and b is: %d", a, b);*/
+
+      //WAP to swap values of two int variables without using third variable
+      /*int a = 990, b = 97;
+
+      a = a + b;
+      b = a - b;
+      a = a - b;
+      printf("Now a is: %d\n", a);
+      printf("Now b is: %d", b);*/
+
+      //WAP to swap values of two int variables without using third variable and +,- operators.
+
+
 
   return 0;
 }
