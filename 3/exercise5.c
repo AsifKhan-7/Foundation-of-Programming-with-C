7219abcb45c89db7736f4850f0ac0d5fe53dc5af
Asif Khan <asifjamshed4@gmail.com>
2025-12-16 00:01:58 +0530

    Exercise - 5 is complete

--- 3/exercise5.c
+++ 3/exercise5.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-// int main() {
+int main() {
 //   int num = 456, rem,sum = 0;
 //   rem = num % 10;
 //   sum += rem;
@@ -20,14 +20,54 @@
 //   return 0;
 
 
-  int main()
-  {
-  int x;
-    printf("Enter a number");
-    scanf("%d",&x);
-    printf("sum is %d",x/100+x/10%10+x%10);
-    printf("\n");
-    return 0;
-}
+  // int main()
+  // {
+  // int x;
+  //   printf("Enter a number");
+  //   scanf("%d",&x);
+  //   printf("sum is %d",x/100+x/10%10+x%10);
+  //   printf("\n");
+  //   return 0;
 // }
+
+  // WAP to find ASCII code of the character '+'.
+  //  char character = '+';
+  //  printf("ACSII code of %c is: %d", character, character);
+
+  //WAP to print size of an int, a folat, a char, and a double type variabl.
+  /* char anyCharacter = 'A';
+   int anyInteger = 10;
+   float PI = 3.14;
+   double anyDouble = 3.144444;
+   printf(" size of character %c is %d byte\n ",anyCharacter, sizeof(anyCharacter));
+   printf("size of integer %d is %d bytes\n ",anyInteger, sizeof(anyInteger));
+   printf("size of float %f is %d bytes\n ",PI, sizeof(PI));
+   printf("size of double %lf is %d bytes ",anyDouble, sizeof(anyDouble));*/
+
+   /*WAP to make the last digit of a number stored in a variable as zero.
+   (Example - if x = 2345 then make it x = 2340)
+   */
+   /*int number, lastDigit;
+   printf("Please enter any number: ");
+   scanf("%d", &number);
+   lastDigit = number % 10;
+   number = number - lastDigit;
+   printf("Now number is: %d ",number);*/
+
+   /*
+     WAP to input a number from the user and also input a digit. Append the digit to the number and print the resulting number.
+     (Example - number = 234 and digit = 9 then the resulting number = 2349)
+    */
+
+    /*int num, digit;
+
+    printf("Please enter any number: ");
+    scanf("%d", &num);
+    printf("Please enter any digit: ");
+    scanf("%d", &digit);
+    num = (num * 10) + digit;
+    printf("Now the number is: %d", num);
+    printf("\n");
+   return 0;*/
+}
 
