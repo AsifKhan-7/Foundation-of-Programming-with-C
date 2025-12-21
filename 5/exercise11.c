5206a26295279808a06ffd0a05fde710950c2a42
Asif Khan <asifjamshed4@gmail.com>
2025-12-21 23:02:45 +0530

    Exercise -11 is continue

---
+++ 5/exercise11.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+
+int main() {
+
+  /*
+    WAP to print MySirG 5 times on the screen.
+   */
+
+   /* int i = 1;
+   while (i <= 5)
+   {
+    printf("MySirG: %d", i);
+    printf("\n");
+    i++;
+   } */
+
+   /*
+     WAP to print the first 10 natural numbers.
+    */
+   /* int i = 1;
+
+   while (i <= 10)
+   {
+    printf("%d", i);
+    printf("\n");
+    i++;
+   } */
+
+   /*
+     WAP to print the first 10 natural numbers in reverse order.
+    */
+
+    /* int i = 10;
+
+    while (i >= 1)
+    {
+      printf("%d", i);
+      printf("\n");
+      i--;
+    } */
+
+  /*
+    WAP to print the first 10 odd natural numbers.
+   */
+
+  //  int i = 1;
+
+  //  while (i <= 19)
+  //  {
+  //   if(i % 2 != 0) {
+  //     printf("%d", i);
+  //     printf("\n");
+  //   }
+  //   i++;
+  //  }
+
+  /*
+    WAP to print squares of the first 10 natural numbers.
+   */
+
+   /*int i = 1, count = 1;
+
+   while (count <= 10)
+   {
+    if (i % 2 != 0) {
+      printf("Odd natural number: %d", i);
+      printf("\n");
+      count++;
+    }
+    i++;
+   } */
+
+
+  printf("\n");
+  return 0;
+}
