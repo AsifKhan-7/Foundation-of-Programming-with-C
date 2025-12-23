2239b5a6d5ad9eeae29f1743a84be924d7007ac0
Asif Khan <asifjamshed4@gmail.com>
2025-12-23 23:27:30 +0530

    Exercise - 12 is continuing

---
+++ 5/exercise12.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main() {
+  /*
+    WAP to print MySriG N times on the screen.
+   */
+
+  /* int number;
+  printf("Please enter any number: ");
+  scanf("%d", &number);
+   int i = 1;
+  while (i <= number)
+  {
+    printf("%d: MySirG", i);
+    printf("\n");
+    i++;
+  } */
+
+  /*
+    WAP to print first N natural numbers.
+   */
+
+  /* int number;
+    printf("Please enter any number: ");
+    scanf("%d", &number);
+    int i = 1;
+  while (i <= number)
+  {
+    printf("%d is a natural number ", i);
+    printf("\n");
+    i++;
+  } */
+
+  /*
+    WAP to print the first N odd natural numbers.
+   */
+
+
+
+  printf("\n");
+  return 0;
+}
---
+++ 5/solution11.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+/*
+//Q1
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        printf("MySirG");
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+
+//Q2
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+
+//Q3
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",11-i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+
+//Q4 | 1 3 5 7 9 11 13 15 17 19
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",2*i-1);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+//T=a+(i-1)d
+// =1+(i-1)2
+// =1+2i-2
+// =2i-1
+
+
+//Q5 | 19 17 15 13 11 9 7 5 3 1
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",21-2*i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+    
+//Q6
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",2*i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+
+//Q7
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",22-2*i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q8
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",i*i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q9
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("%d ",i*i*i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
+*/
+//Q10
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+         printf("5 x %d = %d\n",i,5*i);
+         i++;
+    }
+    printf("\n");
+    return 0;
+}
