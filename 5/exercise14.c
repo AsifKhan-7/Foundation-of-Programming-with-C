99721c6a306c15febce7eb036f1b9c998de96251
Asif Khan <asifjamshed4@gmail.com>
2025-12-26 23:58:55 +0530

    Exercise - 14 is continuing

--- 5/exercise14.c
+++ 5/exercise14.c
@@ -2,8 +2,38 @@
 
 int main() {
   /*
-
+  WAP to check whether a given number is a Prime Number or not.
    */
+
+  /* int number;
+   printf("Please enter any number: ");
+   scanf("%d", &number);
+
+   if(number == 2) {
+    printf("%d is a prime number.", number);
+   } else if(number % 1 == 0 && number % number == 0) {
+    printf("%d is a prime number.", number);
+   } else {
+    printf("%d is not a prime number.", number);
+   } */
+
+   /*
+     WAP to calculate factorial of a number.
+    */
+
+  /*  int i = 1, fact = 1, number;
+    printf("Please enter any number: ");
+    scanf("%d", &number);
+
+    while(i <= number) {
+      fact = fact * i;
+      i++;
+    }
+    printf("Factorial of %d is = %d", number, fact); */
+
+    /*
+      WAP to count digits in a given number.
+     */
 
   printf("\n");
   return 0;
