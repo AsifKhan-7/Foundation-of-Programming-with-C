35f3b556f38f9475e243a7d74d482d5d3b92dcfb
Asif Khan <asifjamshed4@gmail.com>
2026-01-01 01:23:01 +0530

    Exercise - 14 is complete

--- 5/exercise14.c
+++ 5/exercise14.c
@@ -49,10 +49,10 @@ int main() {
        WAP to check whether a given number is a Prime number or not.
       */
 
-      int i = 2, isPrime = 1, number;
+      /* int i = 2, isPrime = 1, number;
       printf("Please enter any number: ");
       scanf("%d", &number);
-      if(number == 1) {
+      if(number == 1 || number <= 0) {
         isPrime = 0;
         printf("%d isn't prime.", number);
       }
@@ -67,7 +67,57 @@ int main() {
       }
       if(isPrime == 1) {
         printf("%d is prime.", number);
-      }
+      } */
+
+      /*
+        WAP to calculate LCM of two numbers.
+       */
+
+       /* int a, b, LCM, max;
+       printf("Enter 1st number: ");
+       scanf("%d", &a);
+       printf("Enter 2nd number: ");
+       scanf("%d", &b);
+
+       if(a <= 0 || b <= 0) {
+        printf("Invalid number.");
+        return 0;
+       }
+
+       if(a > b){
+        max = a;
+       } else {
+        max = b;
+       }
+        LCM = max;
+       while(1) {
+
+        if(LCM % a == 0 && LCM % b == 0){
+          printf("%d is LCM.", LCM);
+          break;
+        } else{
+          LCM += max;
+        }
+       } */
+
+  /*
+    WAP to reverse a given number.
+   */
+
+   /* int n, lastDigit, temp, reverseNumber = 0;
+   printf("Please enter any number: ");
+   scanf("%d", &n);
+  temp = n;
+
+  //  while(n > 0) { Only for positive numbers
+   while(n != 0) {
+
+     lastDigit = n % 10;
+     reverseNumber = (reverseNumber * 10) + lastDigit;
+     n = n / 10;
+    }
+    // printf("Reverse number is: %d",reverseNumber);
+    printf("Reverse number of %d is %d.", temp, reverseNumber); */
 
   printf("\n");
   return 0;
